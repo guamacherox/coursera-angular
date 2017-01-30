d6e6dc7eabcf7e34f564b16b3c1eeba2b92d5573
marvalj <juanluismarval@gmail.com>
2017-01-30 01:34:30 -0400

    Module 4 Solution

---
+++ module4-solution/src/menuapp/menudata.service.js
@@ -0,0 +1,45 @@
+(function(){
+	'use strict';
+
+	angular.module('data')
+	.service('MenuDataService', MenuDataService)
+	.constant('ApiBase','https://davids-restaurant.herokuapp.com');
+
+	MenuDataService.$inject =['$http','ApiBase'];
+	function MenuDataService($http, ApiBase){
+
+		var service = this;
+
+		service.getAllCategories = function(){
+
+			return $http({
+				method:"GET",
+				url: (ApiBase + "/categories.json")
+			})
+			.then(function(response){
+				return response.data;
+			})
+			.catch(function(error){
+				return error;
+			});
+		};
+
+		service.getItemsForCategory = function(categoryShortName){
+
+			return $http({
+				method:"GET",
+				url: (ApiBase + "/menu_items.json"),
+				params:{
+					category:categoryShortName
+				}
+			})
+			.then(function(response){
+					return response.data;
+				})
+
+			.catch(function(error){
+					return error;
+			});
+		};
+	}
+})();
